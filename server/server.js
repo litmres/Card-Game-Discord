4a3855cc056694b34e97726c3a95e1eb3d278128
twofist <twofist12345@gmail.com>
2018-11-21 00:59:27 +0100

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -1,20 +1,132 @@
 const Discord = require('discord.js');
 const tokens = require('./tokens.json');
-const client = new Discord.Client();
+const fs = require('fs');
+const bot = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Discord bot Logged in as ${client.user.tag}!`);
+bot.on('ready', () => {
+  console.log(`Discord bot Logged in as ${bot.user.tag}!`);
 });
 
-client.login(tokens.discord.token);
+bot.on("message", msg => {
+    if (msg.author.bot) return;
+	const points = JSON.parse(fs.readFileSync('levelcount.json', 'utf8'));
+	let user = points[msg.author.id];
+	const oneMin = 1000 * 60 * 1;
+	
+	if (!points[msg.author.id]) {
+        points[msg.author.id] = newUser();
+		console.log("created new user");
+    }
+	
+	if(timePast(points[msg.author.id].timeOfLastUpdate, oneMin)){
+		addExp(msg.content, points[msg.author.id]);
+		levelUp(points[msg.author.id]);
+	}
+	
+    const prefix = "!";
+    if (msg.content.toLowerCase().startsWith(prefix + "setDescription".toLowerCase())){
+		const string = msg.content.split(" ")[1];
+		if(string.length < 100){
+			points[msg.author.id].description = string;
+			msg.reply("Description set!");
+		}else{
+			msg.reply("Description not set! description has to be less than 100 characters");
+        }
+		console.log("set description");
+	}
+	
+	const json = JSON.stringify(points, null, "\t");
+    fs.writeFileSync('levelcount.json', json, 'utf8');
+	
+	if (!msg.content.startsWith(prefix)) return;
+	
+	if (msg.content.toLowerCase().startsWith(prefix + "level".toLowerCase())) {
+        const embed = new Discord.RichEmbed()
+            .setAuthor(msg.author.username + "#" + msg.author.discriminator, msg.author.avatarURL)
+            .setColor('#0A599F')
+            .setTimestamp()
+            .addField('level:', `${points[msg.author.id].level}`, true)
+            .addField('experience:', `${points[msg.author.id].exp}`, true)
+            .addField('exptotal:', `${points[msg.author.id].exptotal}`, true)
+            .addField('attack:', `${points[msg.author.id].attack}`, true)
+            .addField('defense:', `${points[msg.author.id].defense}`, true)
+			.addField('description:', `${points[msg.author.id].description}`, true)
+        msg.channel.send({embed});
+		console.log("sent embed!")
+        return;
+	}
+});
 
-function respond(user){
+bot.login(tokens.discord.token);
+
+function newUser(){
+	return {
+            exp: 0,
+            exptotal: 0,
+            level: 1,
+            attack: 0,
+			defense: 1,
+			description: "no description set",
+			timeOfLastUpdate: new Date().getTime(),
+        };
+}
+
+function addExp(msg, user){
+	if (msg.length > 200) {
+		const exp = calcUserExp(msg.length, 20);
+        user.exp += exp;
+        user.exptotal += exp
+    } else {
+        const exp = calcUserExp(msg.length, 5);
+        user.exp += exp;
+        user.exptotal += exp
+    }
+}
+
+function levelUp(user){
+	const levelup = user.level * 20;
+    if (user.exp >= levelup) {
+        const exprest = user.exp - levelup;
+        user.exp = 0 + exprest;
+        user.level++;
+		if(rndNumBetween(0, 1)){
+			user.defense++;
+		}else{
+			user.attack++;
+		}
+    }
+}
+
+function timePast(pastTime, time){
+	return (new Date().getTime() - pastTime < time)?false:true;
+}
+
+function rndNumBetween(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+function calcUserExp(length, amount){
+	return Math.floor((length / amount));
+}
+
+function respond(client){
 	const array = [];
-	client.guilds.forEach(guild => guild.members.forEach(member => array.push({
+	const points = JSON.parse(fs.readFileSync('levelcount.json', 'utf8'));
+	bot.guilds.forEach(guild => guild.members.forEach((member) =>{
+		let user = points[member.user.id];
+		if(!user){
+			user = newUser();
+		}
+		array.push({
         name: member.user.username,
         image: member.user.avatarURL,
-    })));
-	sendDataToClient(user, array);
+		level: user.level,
+		attack: user.attack,
+		defense: user.defense,
+		description: user.description,
+    })
+	}));
+	sendDataToClient(client, array);
 }
 
 const WebSocket = require('ws');
