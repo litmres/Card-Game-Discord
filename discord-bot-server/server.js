3743c46e9e14deda873af68ad0626c1e7f619ee0
twofist <twofist12345@gmail.com>
2018-12-08 19:09:22 +0100

    readd ws

--- discord-bot-server/server.js
+++ discord-bot-server/server.js
@@ -171,3 +171,60 @@ function rndNumBetween(min,max){
 function calcUserExp(length, amount){
 	return Math.floor((length / amount));
 }
+
+function respond(client){
+	const array = [];
+    const points = getCollectionObj();
+    console.log("reading json:", points);
+	bot.guilds.forEach(guild => guild.members.forEach((member) =>{
+		let user = points[member.user.id];
+		if(!user){
+            user = newUser(member.user.username, member.user.avatarURL);
+            console.log("creating dummy user");
+		}
+		array.push({
+        name: member.user.username,
+        image: member.user.avatarURL,
+		level: user.level,
+		attack: user.attack,
+		defense: user.defense,
+		description: user.description,
+    })
+	}));
+	sendDataToClient(client, array);
+}
+const WebSocket = require('ws');
+const PORT = process.env.PORT || 6060;
+const wss = new WebSocket.Server({
+    port: PORT
+});
+ wss.on('connection', function connection(ws, req) {
+    console.log("someone reached the server", getDateTime());
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    const user = {
+        ip: ip,
+        socket: ws,
+    };
+     ws.on('message', function(data) {
+        respond(user);
+    });
+});
+ function getDateTime() {
+    const date = new Date();
+     let hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+     let min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+     let sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+     let year = date.getFullYear();
+     let month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+     let day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+     return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
+}
+ function sendDataToClient(user, data){
+	console.log("sending", data)
+    user.socket.send(JSON.stringify(data));
+ }
